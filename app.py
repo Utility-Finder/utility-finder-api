b876d4e381ea9529c2270f4add9ba0d5e4ec1fd3
jasmaa <jasonmaa3955@gmail.com>
2021-03-13 14:16:06 -0500

    Add dummy static image upload for add utility

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+import uuid
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,8 +15,8 @@ def list():
             'id': 'abcde12345',
             'type': 2,
             'imageURL': 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-            'lat': 0.0,
-            'lon': 0.0,
+            'lat': 38.893452,
+            'lon': -77.014709,
             'description': 'Nice and warm',
             'rating': 4.35,
         },
@@ -24,15 +25,34 @@ def list():
 
 @app.route('/utility', methods=['POST'])
 def add_utility():
-    # TODO
-    return jsonify('Add utility'), 200
+
+    id = str(uuid.uuid4())
+    lat = float(request.form['lat'])
+    lon = float(request.form['lon'])
+    description = request.form['description']
+
+    file = request.files['image']
+    file.save(f'static/{id}.png')  # assume one image for now
+
+    # TODO: save data into db
+    utility = {
+        'id': id,
+        'lat': lat,
+        'lon': lon,
+        'description': description,
+        'imageURL': f'static/{id}.png',
+    }
+
+    print(utility)
+
+    return jsonify(utility), 200
 
 
 @app.route('/utility/<id>', methods=['PUT'])
 def edit_utility(id):
     # TODO
     return jsonify(f'Edit utility {id}'), 200
-    
+
 
 @app.route('/utility/<id>/rate', methods=['POST'])
 def rate(id):
