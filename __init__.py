b30b77344026bc5365d624732dc05306d717a39b
jasmaa <jasonmaa3955@gmail.com>
2021-03-22 16:02:58 -0400

    Switch to pgsql

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
 
@@ -8,8 +12,16 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_SERVICE = os.environ.get('DB_SERVICE')
+    DB_PORT = os.environ.get('DB_PORT')
+
+    pg_uri = f'postgresql://{DB_USER}:{DB_PASS}@{DB_SERVICE}:{DB_PORT}/{DB_NAME}'
+
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = pg_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
